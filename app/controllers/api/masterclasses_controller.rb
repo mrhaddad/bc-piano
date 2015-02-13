a848843409e0eb6b21f484945f5a04fa55604ae1
Michael Haddad <michael@ludditetechnology.com>
2015-02-12 19:57:50 -0500

    Fix mobile

--- app/controllers/api/masterclasses_controller.rb
+++ app/controllers/api/masterclasses_controller.rb
@@ -1,6 +1,4 @@
 class Api::MasterclassesController < ApplicationController
-  before_filter :authenticate_user!
-
   def index
     render json: ActiveModel::ArraySerializer.new(Masterclass.order(:date).includes(:performances), scope: current_user)
   end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,2 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_user!
 end
